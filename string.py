c3057e05497ecc297173f60f2224422b3a43d14f
jayesh mathur <jayeshmathur98@gmail.com>
2026-06-06 20:09:06 +0530

    first commit

---
+++ string.py
@@ -0,0 +1,27 @@
+# name = "Jayesh"
+# print(name[::-1])
+# print(len(name))
+# print(name.upper())
+# print(name.lower())
+
+
+name = "pyTHon"
+lower = ""
+upper = ""
+for i in name:
+    if i.islower():
+        lower = lower + i
+    else:
+         upper = upper + i
+print(lower+upper)
+
+
+
+
+
+
+
+
+
+
+
