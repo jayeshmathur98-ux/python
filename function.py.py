f8a6349b5f76295598b45421d5212b4e48e4c3c7
jayeshmathur98-ux <jayeshmathur98@gmail.com>
2026-06-17 15:43:56 +0530

    Add files via upload

---
+++ function.py.py
@@ -0,0 +1,374 @@
+# function ka namm = variable naam
+
+# def greet():#function defining
+#     print('Hello world')
+# greet()#function cal
+
+# def greet(name):
+#     name = input('enter your name:')
+#     print(f'hello {name}!')
+# greet('name')    
+
+
+# def greet(name):
+#     print(f'hello {name}!')
+
+# greet('dhoni')
+# greet('virat')
+
+
+#ques print greatest between 2 variables.import re
+
+
+# import copy
+
+
+# def greatest ():
+#     a = 10
+#     b = 5
+#     if a > b :
+#         print(a)
+#     else:
+#         print(b)
+
+
+
+
+# def pallindrone (name):
+#     a = "SHERYIANS"
+#     b = ""
+# for i in range(len(a)-1,-1,-1):
+#     b = b + a[i]
+# if b == a:
+#     print("your string is pallindrome")
+# else:
+#     print("its not a pallindrome")    
+
+# n = 10
+# for i in range(1,11,1):
+#     if n % i == 0:
+#         print(i)
+    
+
+# return statement
+# def add():
+#     a = 10
+#     b = 20
+#     return a + b
+# print(add())    
+
+
+# def greet():
+#     return "hello"
+# print(greet())
+
+
+
+# def greet():
+#     return "hello"
+# def add():
+#     print(greet())
+#     a = 10
+#     b = 20
+#     return a + b
+# print(add())
+
+
+
+
+# def add(a,b):#function defone krte waqt parameter.
+#     print(a+b)
+# add(10,20) #function call krte waqt argumnets.   
+
+
+# def add(a : bool,b : bool):
+#     print(a + b)
+# add(False,False)
+
+
+# def add (a,b=0):#default parameters
+#     print(a + b)
+# add(10)  
+# add(10,20)  
+
+
+# except an parameter named as 'n' and print factorial of 'n' using function
+
+# def fact():
+#     n = int(input("enter a number"))
+#     fact = 1
+#     for i in range(1,n+1):
+#         fact *= i
+#     print(fact)     
+# fact()
+
+
+
+#keyword arguments
+# def info(name,gender ,age,address):
+#     print(name)
+#     print(gender)
+#     print(age)
+#     print(address)
+
+# info(name="mukesh",gender='m',age=52,address='bhopal')    
+
+
+# def palindrome(num):
+#     copy = num
+# reverse = 0
+# while num > 0:
+#     last = num % 10
+#     reverse = reverse * 10 + last
+#     num = num // 10
+# if copy == reverse:
+#     print(f"{copy} is a pallindrone number")
+# else:
+#     print(f"{copy} is not a palllindrome number") 
+# print(palindrome(123))
+
+
+
+
+# recursion
+
+# def display(num):
+#     if num > 0:
+#         return
+#     print(num)
+#     display(num + 1)
+# display(1)
+
+
+# def factorial(num):
+#     if num == 0 or num ==1:
+#         return 1
+#     else:
+#         return n * factorial(n - 1)
+#     print(factorial(5))
+
+
+# def sum(a,b):
+#     print(f"the sum of your number is {a + b}")
+# sum(12,12)
+# sum(45,45)    
+
+
+
+# def add(a,b):#positional parameter 
+#     return a + b
+# print(add(3,5))#positional parameter 
+
+
+
+
+# def hello(name,age,place):#keyword parameter
+#     print(f"my name is {name} and i am only {age} years old and   i live in {place}")
+# hello(age = 19,name = "jayesh",place = "Bhopal") #keyword argument
+
+
+
+# def sum(a,b=45):#default parameter 
+#     print(f"the sum is {a+b}")
+# sum(12) #default argument    
+
+
+
+# def pallindrome(st):
+#     rev = ""
+#     for i in range(len(st)-1,-1,-1):
+#         rev = rev + st[i]
+#     if rev == st:
+#         print(f"{st} is a pallindrome")
+#     else :
+#         print(f"{st} is not a pallindrome")
+# pallindrome("NAMAN")        
+# pallindrome("jayeshseyaj")
+# pallindrome("cursor")    
+
+
+# def hello():
+#     return "hello how are you"
+# print(hello())
+
+#lamda , krgs , kwkrgs we studies after data structures
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
